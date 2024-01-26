cb270a52ec75fcc9dbd1001de66b2d76d94c0d24
junho kim <kesu9251@naver.com>
2024-01-26 11:21:49 +0900

    57page 까지
    Repository 및 form 선언
    
    Signed-off-by: junho kim <kesu9251@naver.com>

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,12 +1,15 @@
 package com.example.firstproject.controller;
 
 import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
+    private ArticleRepository articleRepository;
     @GetMapping("/articles/new")
     public String newArticleForm() {
         return "articles/new";
@@ -15,6 +18,8 @@ public class ArticleController {
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
         System.out.println(form.toString());
+        Article article = form.toEntity();
+        Article saved = articleRepository.save(article);
         return "";
     }
 }
