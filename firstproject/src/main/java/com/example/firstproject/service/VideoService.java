cb30d2826ddcc4e6301cd3d2aacadf40a1bad696
junho kim <kesu9251@naver.com>
2024-02-06 21:17:19 +0900

    동영상을 받아서 폴더 생성 및 저장.
    1. 게시글제목 이름으로 폴더 생성
    2. ffmpeg로 실행하려고 했으나 수정 필요.
    
    Signed-off-by: junho kim <kesu9251@naver.com>

--- firstproject/src/main/java/com/example/firstproject/service/VideoService.java
+++ firstproject/src/main/java/com/example/firstproject/service/VideoService.java
@@ -75,6 +75,55 @@ public class VideoService{
         };
     }
 
+    // 파일명에서 확장자를 제외한 부분 추출하는 메서드
+    private static String getFolderName(String filename) {
+        int lastIndex = filename.lastIndexOf('.');
+        if (lastIndex != -1) {
+            return filename.substring(0, lastIndex);
+        } else {
+            return filename; // 확장자가 없는 경우 그대로 반환
+        }
+    }
+
+    public boolean changeTom3u8(String filePath) {
+
+        Path folderPath = Paths.get(getFolderName(filePath));
+        log.info(folderPath.toString());
+        try {
+            // 폴더를 생성할 경로를 Path 객체로 변환
+            // 폴더 생성
+            Files.createDirectories(folderPath);
+            log.info("폴더가 생성되었습니다.");
+        } catch (Exception e) {
+            log.info("폴더 생성 중 오류가 발생했습니다.");
+            e.printStackTrace();
+            return false;
+        }
+
+        String ffmpegCmd = "ffmpeg -i \"" + filePath + "\" -codec: copy -start_number 0 -hls_time 10 -hls_list_size 0 -f hls \"" + folderPath.toString() + "\\index.m3u8\"";
+        log.info(ffmpegCmd);
+        try {
+            // 명령어 실행
+            Process process = Runtime.getRuntime().exec(ffmpegCmd);
+
+            // 실행 결과 출력
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                log.info(line);
+            }
+
+            // 프로세스 종료 대기
+            int exitCode = process.waitFor();
+            log.info("FFmpeg 실행 종료 코드: " + exitCode);
+        } catch (IOException | InterruptedException e) {
+            log.info(e.toString());
+            return false;
+        }
+        log.info("완료되었습니다.");
+        return true;
+    }
+
     public void save_file(MultipartFile videoFile, Article savedArticle) {
         String uploadDir = "D:/new";
         if (videoFile.isEmpty()) {
@@ -107,6 +156,7 @@ public class VideoService{
             // 파일 저장 성공 시 추가 작업 수행 가능
             Videos videos = new Videos(null, savedArticle.getId(), filePath.toString(), savedArticle.getTitle());
             videoRepository.save(videos);
+            changeTom3u8(filePath.toString());
             log.info("파일이 성공적으로 업로드되었습니다.");
         } catch (IOException e) {
             log.info(e.toString());
